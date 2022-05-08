1434438907f7ab93b991ea19d33f9358348e7950
MehruzSaif <mehruz.saif@g.bracu.ac.bd>
2022-05-08 19:51:01 +0600

    insert upsert method

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 const app = express();
 
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -28,25 +28,44 @@ async function run() {
             res.send(items);
         });
 
-        app.get('/item/:id', async(req, res) => {
+        app.get('/item/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const item = await itemCollection.findOne(query);
             res.send(item);
         });
 
         // POST
-        app.post('/item', async(req, res) => {
+        app.post('/item', async (req, res) => {
             const newItem = req.body;
             const result = await itemCollection.insertOne(newItem);
             res.send(result);
         });
 
         // DELETE
-        app.delete('/item/:id', async(req, res) =>{
+        app.delete('/item/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        // Update quantity
+        app.put("/item/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedItem = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDocument = {
+                $set: updatedItem
+            };
+            const result = await itemCollection.updateOne(
+                filter,
+                updateDocument,
+                options
+            );
+            console.log("updating", id);
             res.send(result);
         });
 
